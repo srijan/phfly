c3090d4f73341b5eb85b791537064dda566952e9
Srijan Choudhary <srijan.c@greyorange.com>
2022-09-23 11:00:33 +0530

    Add a simple TCP echo server

---
+++ src/ph0.erl
@@ -0,0 +1,22 @@
+-module(ph0).
+-behaviour(ranch_protocol).
+
+-export([start_link/3]).
+-export([init/3]).
+
+start_link(Ref, Transport, Opts) ->
+    Pid = spawn_link(?MODULE, init, [Ref, Transport, Opts]),
+    {ok, Pid}.
+
+init(Ref, Transport, _Opts = []) ->
+    {ok, Socket} = ranch:handshake(Ref),
+    loop(Socket, Transport).
+
+loop(Socket, Transport) ->
+    case Transport:recv(Socket, 0, 60000) of
+        {ok, Data} when Data =/= <<4>> ->
+            Transport:send(Socket, Data),
+            loop(Socket, Transport);
+        _ ->
+            ok = Transport:close(Socket)
+    end.
