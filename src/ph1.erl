5bf34a8be49db11b5823fa75a8f3b2b0b839bdc5
Srijan Choudhary <srijan.c@greyorange.com>
2022-09-24 08:14:47 +0530

    gen_server based implementation

--- src/ph1.erl
+++ src/ph1.erl
@@ -1,39 +1,61 @@
 -module(ph1).
 -behaviour(ranch_protocol).
+-behaviour(gen_server).
 
+%% For ranch_protocol behaviour
 -export([start_link/3]).
--export([init/3]).
+
+%% For gen_server behaviour
+-export([
+    init/1,
+    handle_continue/2,
+    handle_info/2
+]).
+
+-record(state, {ref, transport, socket}).
 
 start_link(Ref, Transport, Opts) ->
-    Pid = spawn_link(?MODULE, init, [Ref, Transport, Opts]),
-    {ok, Pid}.
+    gen_server:start_link(?MODULE, {Ref, Transport, Opts}, []).
 
-init(Ref, Transport, _Opts = []) ->
+init({Ref, Transport, _Opts = []}) ->
+    {ok, #state{ref = Ref, transport = Transport}, {continue, do_handshake}}.
+
+handle_continue(do_handshake, State = #state{ref = Ref, transport = Transport}) ->
     {ok, Socket} = ranch:handshake(Ref),
-    ok = inet:setopts(Socket, [{packet, line}, {recbuf, 1024*1024}]),
-    loop(Socket, Transport).
+    %% Line packaging also truncates at recbuf size, so increasing it as well
+    ok = Transport:setopts(Socket, [{active, once}, {packet, line}, {recbuf, 1024 * 1024}]),
+    {ok, {ClientIP, _}} = inet:peername(Socket),
+    io:format("Client connected: ~p~n", [ClientIP]),
+    {noreply, State#state{socket = Socket}}.
 
-loop(Socket, Transport) ->
-    case Transport:recv(Socket, 0, 60000) of
-        {ok, Data} when Data =/= <<4>> ->
-            SplitData = string:split(Data, <<"\n">>, all),
-            lists:foreach(
-              fun(<<>>) -> ok;
-                 (D) ->
-                      Reply = get_reply(D),
-                      Transport:send(Socket, Reply)
-              end, SplitData),
-            loop(Socket, Transport);
-        _ ->
-            ok = Transport:close(Socket)
-    end.
+handle_info({tcp, Socket, Line}, State = #state{socket = Socket, transport = Transport}) ->
+    io:format("Received ~p bytes~n", [erlang:size(Line)]),
+    SplitData = string:split(Line, <<"\n">>, all),
+    lists:foreach(
+        fun
+            (<<>>) ->
+                ok;
+            (D) ->
+                Reply = get_reply(D),
+                Transport:send(Socket, Reply)
+        end,
+        SplitData
+    ),
+    Transport:setopts(Socket, [{active, once}]),
+    {noreply, State};
+handle_info({tcp_closed, Socket}, State = #state{socket = Socket, transport = Transport}) ->
+    io:format("TCP Close~n"),
+    {stop, normal, State};
+handle_info({tcp_error, _, Reason}, State) ->
+    io:format("TCP Error~n"),
+    {stop, Reason, State}.
 
 get_reply(Data) ->
     case thoas:decode(string:trim(Data)) of
         {ok, #{<<"method">> := <<"isPrime">>, <<"number">> := Number}} when is_number(Number) ->
             io:format("Number: ~p~n", [Number]),
             Response = #{<<"method">> => <<"isPrime">>, <<"prime">> => is_prime(Number)},
-            <<(thoas:encode(Response))/binary, 10>>;
+            [thoas:encode(Response), <<"\n">>];
         _ ->
             io:format("Malformed: ~p~n", [Data]),
             <<"malformed\n">>
