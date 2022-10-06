97463c1b59ea2f9eea62cb758c1e5db36410561b
Srijan Choudhary <srijan.c@greyorange.com>
2022-10-07 00:11:16 +0530

    Simplify ph4

--- src/ph4.erl
+++ src/ph4.erl
@@ -26,43 +26,31 @@ handle_cast(_Msg, State) ->
 
 handle_info({udp, Socket, FromIp, FromPort, Command}, State = #state{socket = Socket, kv = KV}) ->
     io:format("Got command: ~p~n", [Command]),
-    NewKV = handle_command(Command, Socket, {FromIp, FromPort}, KV),
+    Split = string:split(Command, "="),
+    NewKV = handle_command(Split, Socket, {FromIp, FromPort}, KV),
     {noreply, State#state{kv = NewKV}}.
 
 %% internal functions
 
 start_listening() ->
     Port = application:get_env(phfly, udp_port, 8080),
-    ListenIP =
+    {ok, ListenIP} =
         case os:getenv("FLY_APP_NAME") of
             false ->
-                {0, 0, 0, 0};
+                {ok, {0, 0, 0, 0}};
             _AppName ->
-                {ok,
-                    {hostent, "fly-global-services", ["fly-global-services"], inet, 4, [
-                        FlyGlobalServicesIP
-                    ]}} = inet:gethostbyname("fly-global-services"),
-                FlyGlobalServicesIP
+                inet:getaddr("fly-global-services", inet)
         end,
     {ok, Socket} = gen_udp:open(Port, [{ip, ListenIP}]),
     io:format("UDP Server listening on ~p:~p~n", [ListenIP, Port]),
     Socket.
 
-handle_command("version", Socket, Source, KV) ->
-    gen_udp:send(Socket, Source, "version=v2"),
+handle_command(["version"], Socket, Source, KV) ->
+    gen_udp:send(Socket, Source, "version=v3"),
     KV;
-handle_command(Command, Socket, Source, KV) ->
-    NewKV =
-        case string:split(Command, "=") of
-            [Key] ->
-                case maps:get(Key, KV, undefined) of
-                    undefined ->
-                        gen_udp:send(Socket, Source, [Key, "="]);
-                    Value ->
-                        gen_udp:send(Socket, Source, [Key, "=", Value])
-                end,
-                KV;
-            [Key, Value] ->
-                maps:put(Key, Value, KV)
-        end,
-    NewKV.
+handle_command([Key], Socket, Source, KV) ->
+    Value = maps:get(Key, KV, ""),
+    gen_udp:send(Socket, Source, [Key, "=", Value]),
+    KV;
+handle_command([Key, Value], _Socket, _Source, KV) ->
+    maps:put(Key, Value, KV).
